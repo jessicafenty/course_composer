efc0380daefe4782482e0faefa34bea77feb3b22
jessicafenty <jssicamartinsnunes@gmail.com>
2017-05-15 22:17:37 -0300

    second commit

---
+++ App/Jessica/SON/HelloPackage.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Jessica
+ * Date: 15/05/2017
+ * Time: 22:09
+ */
+namespace Jessica\SON;
+
+class HelloPackage
+{
+    public function getHello(){
+        return "Hello Package";
+    }
+}
